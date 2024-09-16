b770939c4ba9f3fad3582e2f3aa01deebd74a959
moolanar <moolanar@gmail.com>
2024-09-16 14:02:28 +0800

    Update CheckPrimeNo.java

--- CheckPrimeNo.java
+++ CheckPrimeNo.java
@@ -1,39 +1,57 @@
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.time.LocalDateTime;
-
+import java.io.*;
 public class CheckPrimeNo {
 	
 	public static void main(String s[]) { 
 		
 		BigInteger ONE = new BigInteger("1");
-		BigInteger INC = new BigInteger("1000000");  // iM * 10 threads
+		BigInteger TEN = new BigInteger("10");
+		BigInteger INC = new BigInteger("200000000");  // 1M * 10 threads
 		
-		System.out.println(LocalDateTime.now()); 
+		//32769132993266709549961988190834461413177642967992942539798288533
+		//32769132993266709549961988190834461413177642967992942540000000001
+		//32769132993266709549961988190834461413166610000000000000000000000
+		//32769132993266709549961988190834461413177642967992942531000000000
 		
-		//BigInteger start = new BigInteger("10000000001");
 		
-		//
-		BigInteger factorarg = new BigInteger("10000000").multiply(new BigInteger(s[0])) ;  // 10 M per process 
-		BigInteger start = new BigInteger("10000000000").add(factorarg);
+		BigInteger no = new BigInteger("114381625757888867669235779976146612010218296721242362562561842935706935245733897830597123563958705058989075147599290026879543541");
+		
+		BigInteger input = new BigInteger("32769132993266709549961988190834461413177642967992942520000000000");
+		
+		System.out.println(LocalDateTime.now());  
+
+		BigInteger factorarg = INC.multiply(new BigInteger(s[0])).multiply(TEN) ;  // 10 M per process 
+		
+		System.out.println(" input no      " + input);
+		System.out.println(" factorarg no  " + factorarg);
+		
+		BigInteger start = input.add(factorarg);
+
 		start = start.add(ONE);
 		BigInteger end = null;
 		
 		
 		
 		for(int i = 0; i< 10; i++ ){			
-			end = start.add(INC).subtract(ONE);			
-			new MyThread("T"+i, start, end).start();			
+			end = start.add(INC).subtract(ONE);		
+			
+            System.out.println(i + " start no  " + start);
+            System.out.println(i + " end no    " + end);
+			
+			new MyThread(s[0] + "_" +i, start, end, no).start();			
 			start = start.add(INC);			
 		}
 		
-		System.out.println("end no  " + end);
+		
 	}
 
 }
 
 class MyThread extends Thread {
 	String s = null;
+	BigInteger input =  null;
 	BigInteger bi1 =  null;
  	BigInteger bi2 =  null;
 	BigInteger d = new BigInteger("1");
@@ -46,12 +64,19 @@ class MyThread extends Thread {
 	StringBuilder sb = new StringBuilder();
 	
 	long n = 10000L;
-	ArrayList al = new ArrayList();
+	FileWriter fw = null;
 	
-	MyThread(String s , BigInteger bi1, BigInteger bi2 ) {
+	MyThread(String s , BigInteger bi1, BigInteger bi2, BigInteger input ) {
 		this.s = s;
 		this.bi1 = bi1;
 		this.bi2 = bi2;
+		this.input = input;
+
+        try {
+            //fw = new FileWriter(new File(s+".txt"));
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
 	}
 	
 	public void run() { 
@@ -60,23 +85,56 @@ class MyThread extends Thread {
 			isPrime(bi1);
 			bi1 = bi1.add(ONE);
 		}
-		
+
+        try {
+			if(sb.length() > 0) {
+				if(fw == null) {
+					fw = new FileWriter(new File(s+".txt"));
+				}
+				fw.write(sb.toString());
+				fw.flush();
+				sb.delete(0, sb.length());
+			}			
+			    
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		System.out.println(LocalDateTime.now());  
 	}
 	
 	 boolean isPrime(BigInteger bi) {	
 		
 		 ++c;
-		if(c % 5000 == 0 ) {
+		if(c % 100000 == 0 ) {
 			
-			System.out.print(sb.toString());
-			sb.delete(0, sb.length());
-			c=0;
+			//System.out.print(sb.toString() + " / " + sb.length());
+            try {
+				if(sb.length() > 0) {
+					if(fw == null) {
+						fw = new FileWriter(new File(s+".txt"));
+					}
+					fw.write(sb.toString());
+					sb.delete(0, sb.length());
+					c=0;
+				}
+                
+            }catch(Exception e) {
+                e.printStackTrace();
+            }
+			
+			
 		}
 
-		r = null;
 		
 		r=bi.mod(TWO);
 		if((r.compareTo(ZERO)) == 0) {
+			//System.out.println("Not Prime " + bi  + " / " + r  + " / " + d);
+				return false;
+		}
+		
+		r = input.mod(bi);		
+		if((r.compareTo(ZERO)) != 0) {
 			//System.out.println("Not Prime " + bi  + " / " + r  + " / " + d);
 				return false;
 		}
@@ -91,7 +149,9 @@ class MyThread extends Thread {
 			}			
 		}
 		
-		sb.append(bi + "\n");
+		//sb.append(bi1 +" / " +  bi2 + " - " +  bi + "\n");
+		System.out.println("Possible prime is : " + bi );
+        sb.append(  bi + "\n");
 		++p;
 
 		
