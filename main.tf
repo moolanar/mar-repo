5b9a65e6ce437536ed2320bc1df939b8947983ca
moolanar <moolanar@gmail.com>
2024-09-18 07:28:00 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,13 +10,89 @@ terraform {
 }
 
 provider "aws" {
-  region  = "ap-southeast-2"
-  access_key="121212"
-  secret_key="121212"
+  region  = "ap-southeast-2" 
+}
+
+resource "aws_vpc" "Altschool_vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  tags = {
+    Name = "Altschool_vpc"
+  }
+}
+
+resource "aws_internet_gateway" "Altschool_internet_gateway" {
+  vpc_id = aws_vpc.Altschool_vpc.id
+  tags = {
+    Name = "Altschool_internet_gateway"
+  }
+}
+
+resource "aws_route_table" "Altschool-route-table-public" {
+  vpc_id = aws_vpc.Altschool_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.Altschool_internet_gateway.id
+  }
+  tags = {
+    Name = "Altschool-route-table-public"
+  }
+}
+
+resource "aws_route_table_association" "Altschool-public-subnet1-association" {
+  subnet_id      = aws_subnet.Altschool-public-subnet1.id
+  route_table_id = aws_route_table.Altschool-route-table-public.id
+}
+
+resource "aws_route_table_association" "Altschool-public-subnet2-association" {
+  subnet_id      = aws_subnet.Altschool-public-subnet2.id
+  route_table_id = aws_route_table.Altschool-route-table-public.id
+}
+
+resource "aws_subnet" "Altschool-public-subnet1" {
+  vpc_id                  = aws_vpc.Altschool_vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "eu-west-2a"
+  tags = {
+    Name = "Altschool-public-subnet1"
+  }
+}
+
+resource "aws_subnet" "Altschool-public-subnet2" {
+  vpc_id                  = aws_vpc.Altschool_vpc.id
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "eu-west-2b"
+  tags = {
+    Name = "Altschool-public-subnet2"
+  }
+}
+
+resource "aws_network_acl" "Altschool-network_acl" {
+  vpc_id     = aws_vpc.Altschool_vpc.id
+  subnet_ids = [aws_subnet.Altschool-public-subnet1.id, aws_subnet.Altschool-public-subnet2.id]
+
+	ingress {
+	    rule_no    = 100
+	    protocol   = "-1"
+	    action     = "allow"
+	    cidr_block = "0.0.0.0/0"
+	    from_port  = 0
+	    to_port    = 0
+	  }
+	  egress {
+	    rule_no    = 100
+	    protocol   = "-1"
+	    action     = "allow"
+	    cidr_block = "0.0.0.0/0"
+	    from_port  = 0
+	    to_port    = 0
+	  }
 }
 
 resource "aws_security_group" "sg1" {
-  name        = "my_sec_grp"
+  name = "my_sec_grp"
   description = "Allow inbound SSH and HTTP traffic"
   
   ingress {
