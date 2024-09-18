c29cd3aca305a8c046da14f4a3117d0e750a997c
moolanar <moolanar@gmail.com>
2024-09-18 12:29:05 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,105 +1,100 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.16"
-    }
-  }
+#terraform {
+#  required_providers {
+#    aws = {
+#      source  = "hashicorp/aws"
+#      version = "~> 4.16"
+#    }
+#  }
+#  required_version = ">= 1.2.0"
+#}
 
-  required_version = ">= 1.2.0"
-}
 
 provider "aws" {
   region  = "ap-southeast-2" 
 }
 
-resource "aws_vpc" "Altschool_vpc" {
+resource "aws_vpc" "mar_vpc" {
   cidr_block = "10.0.0.0/16"
   enable_dns_hostnames = true
+  enable_dns_support = true
   tags = {
-    Name = "Altschool_vpc"
+    Name = "mar_vpc"
   }
 }
 
-resource "aws_internet_gateway" "Altschool_internet_gateway" {
-  vpc_id = aws_vpc.Altschool_vpc.id
+resource "aws_internet_gateway" "mar_internet_gateway" {
+  vpc_id = aws_vpc.mar_vpc.id
   tags = {
-    Name = "Altschool_internet_gateway"
+    Name = "mar_internet_gateway"
   }
 }
 
-resource "aws_route_table" "Altschool-route-table-public" {
-  vpc_id = aws_vpc.Altschool_vpc.id
+resource "aws_route_table" "mar-route-table-public" {
+  vpc_id = aws_vpc.mar_vpc.id
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.Altschool_internet_gateway.id
+    gateway_id = aws_internet_gateway.mar_internet_gateway.id
   }
   tags = {
-    Name = "Altschool-route-table-public"
+    Name = "mar-route-table-public"
   }
 }
 
-resource "aws_route_table_association" "Altschool-public-subnet1-association" {
-  subnet_id      = aws_subnet.Altschool-public-subnet1.id
-  route_table_id = aws_route_table.Altschool-route-table-public.id
+resource "aws_route_table_association" "mar-public-subnet1-association" {
+  subnet_id      = aws_subnet.mar-public-subnet1.id
+  route_table_id = aws_route_table.mar-route-table-public.id
 }
 
-resource "aws_route_table_association" "Altschool-public-subnet2-association" {
-  subnet_id      = aws_subnet.Altschool-public-subnet2.id
-  route_table_id = aws_route_table.Altschool-route-table-public.id
+resource "aws_route_table_association" "mar-public-subnet2-association" {
+  subnet_id      = aws_subnet.mar-public-subnet2.id
+  route_table_id = aws_route_table.mar-route-table-public.id
 }
 
-resource "aws_subnet" "Altschool-public-subnet1" {
-  vpc_id                  = aws_vpc.Altschool_vpc.id
-  cidr_block              = "10.0.1.0/24"
+resource "aws_route_table_association" "mar-public-subnet3-association" {
+  subnet_id      = aws_subnet.mar-public-subnet3.id
+  route_table_id = aws_route_table.mar-route-table-public.id
+}
+
+resource "aws_subnet" "mar-public-subnet1" {
+  vpc_id = aws_vpc.mar_vpc.id
+  cidr_block = "10.0.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone       = "eu-west-2a"
+  availability_zone = "ap-southeast-2a"
   tags = {
-    Name = "Altschool-public-subnet1"
+    Name = "mar-public-subnet1"
   }
 }
 
-resource "aws_subnet" "Altschool-public-subnet2" {
-  vpc_id                  = aws_vpc.Altschool_vpc.id
-  cidr_block              = "10.0.2.0/24"
+resource "aws_subnet" "mar-public-subnet2" {
+  vpc_id = aws_vpc.mar_vpc.id
+  cidr_block = "10.0.2.0/24"
   map_public_ip_on_launch = true
-  availability_zone       = "eu-west-2b"
+  availability_zone = "ap-southeast-2b"
   tags = {
-    Name = "Altschool-public-subnet2"
+    Name = "mar-public-subnet2"
   }
 }
 
-resource "aws_network_acl" "Altschool-network_acl" {
-  vpc_id     = aws_vpc.Altschool_vpc.id
-  subnet_ids = [aws_subnet.Altschool-public-subnet1.id, aws_subnet.Altschool-public-subnet2.id]
-
-	ingress {
-	    rule_no    = 100
-	    protocol   = "-1"
-	    action     = "allow"
-	    cidr_block = "0.0.0.0/0"
-	    from_port  = 0
-	    to_port    = 0
-	  }
-	  egress {
-	    rule_no    = 100
-	    protocol   = "-1"
-	    action     = "allow"
-	    cidr_block = "0.0.0.0/0"
-	    from_port  = 0
-	    to_port    = 0
-	  }
+resource "aws_subnet" "mar-public-subnet3" {
+  vpc_id = aws_vpc.mar_vpc.id
+  cidr_block = "10.0.3.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = "ap-southeast-2c"
+  tags = {
+    Name = "mar-public-subnet3"
+  }
 }
 
 resource "aws_security_group" "sg1" {
   name = "my_sec_grp"
   description = "Allow inbound SSH and HTTP traffic"
+  vpc_id = aws_vpc.mar_vpc.id
   
   ingress {
     description ="ssh"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
   
@@ -160,26 +155,26 @@ resource "aws_security_group" "sg1" {
   }
   
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
 }
 
 resource "aws_instance" "example_server" {
-  ami = "ami-02346a771f34de8ac"
-  instance_type = "t2.medium"
-  key_name = "putty-kp"
+  ami = "ami-0e8fd5cc56e4d158c"
+  instance_type = "t2.micro"
+  key_name = "jenkins"
   vpc_security_group_ids  = [aws_security_group.sg1.id]
-
+  subnet_id  = aws_subnet.mar-public-subnet1.id
   ebs_block_device {
     device_name = "/dev/xvda"
     volume_size = 8
   }
 
-  user_data = <<EOF
+user_data = <<EOF
 #!/bin/bash
 echo "Copying the SSH Key to the server"
 yum install docker -y
@@ -192,14 +187,14 @@ systemctl restart docker
 #docker run -d -p 3000:3000 grafana/grafana
 #docker run -d -p 8080:8080 --volume /var/lib/docker:/var/lib/docker --volume /var/run:/var/run --volume /sys:/sys --volume /dev/disk:/dev/disk gcr.io/cadvisor/cadvisor
 
-docker network create elastic
-docker run -d --net elastic --name es01 -p 9200:9200 -e "ELASTIC_PASSWORD=pass1234" -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:7.15.0
+#docker network create elastic
+#docker run -d --net elastic --name es01 -p 9200:9200 -e "ELASTIC_PASSWORD=pass1234" -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:7.15.0
 #curl -X PUT "localhost:9200/products"
 #curl -X POST -H 'Content-type:application/json' -d '{"type":"cdlp", "description":"cash deposit" }' http://localhost:9200/products/_doc
 #curl -X GET "localhost:9200/products/_search?pretty" -H 'Content-Type:application/json' -d '{ "query": { "match" : { "type": "cdlp" } } } '
 
-docker run -d --name kib01 -p 5601:5601 docker.elastic.co/kibana/kibana:7.15.0
-docker run -d --name lgs docker.elastic.co/logstash/logstash:7.15.0
+#docker run -d --name kib01 -p 5601:5601 docker.elastic.co/kibana/kibana:7.15.0
+#docker run -d --name lgs docker.elastic.co/logstash/logstash:7.15.0
 
 #-auto-approve
 
